c94f0aa3e86a9c5cc1a4a53e71eb9a606c583f8a
Dimitrios Tsimpoukis <tsiboukisdim@gmail.com>
2022-01-26 14:35:52 +0100

    day25

---
+++ Day25/day25.py
@@ -0,0 +1,102 @@
+from collections import Counter, defaultdict, deque
+from functools import reduce, lru_cache
+from itertools import permutations
+from typing import (
+    List,
+    Tuple,
+    Set,
+    Dict,
+    Iterable,
+    DefaultDict,
+    Optional,
+    Union,
+    Generator,
+)
+from copy import deepcopy
+import re
+from heapq import heappop, heappush
+from time import sleep
+
+from intcode import IntCode
+from termcolor import colored
+
+
+def parse(filename: str) -> IntCode:
+    matrix = []
+    with open(filename, "r") as f:
+        lines = f.read().strip().replace("\n", "")
+
+    l = [int(x) for x in lines.split(",")]
+
+    d = {i: l[i] for i in range(len(l))}
+    return IntCode(d)
+
+
+def str_to_cmd(string):
+    if not string.endswith("\n"):
+        string = string + "\n"
+    cmd = [ord(i) for i in string]
+    return cmd
+
+
+def print_out(intcode):
+    outs = [chr(char) for char in intcode.outs]
+    for grid in "".join(outs).split("\n\n"):
+        print(grid)
+
+
+def run_program(intcode):
+
+    while True:
+        cmd = input("input:")
+        intcode.inputs.extend(str_to_cmd(cmd))
+        intcode.run()
+        print_out(intcode)
+
+
+def main(filename: str) -> Tuple[Optional[int], Optional[int]]:
+    from time import time
+
+    start = time()
+    answer_a, answer_b = None, None
+    intcode = parse(filename)
+    grid = run_program(intcode)
+    print(f"p1: {answer_a}")
+    #p2
+    # intcode = parse(filename)
+    # answer_b = run_program_2(intcode)
+    print(f"p2: {answer_b}")
+    end = time()
+    print(end - start)
+    return answer_a, answer_b
+
+
+if __name__ == "__main__":
+
+    from utils import submit_answer
+    from aocd.exceptions import AocdError
+
+    sample = "sample.txt"
+    inp = "input.txt"
+
+    # sample_a_answer = 1125899906842624
+    # sample_b_answer = 18216
+
+    # answer_a, answer_b = main(sample)
+    # assert (
+    #     answer_a == sample_a_answer
+    # ), f"AnswerA incorrect: Actual: {answer_a}, Expected: {sample_a_answer}"
+    # print("sampleA correct")
+    # if answer_b:
+    #     assert (
+    #         answer_b == sample_b_answer
+    #     ), f"AnswerB incorrect: Actual: {answer_b}, Expected: {sample_b_answer}"
+    #     print("sampleB correct")
+
+    # Test on your input and submit
+    answer_a, answer_b = main(inp)
+    print(f"Your input answers: \nA: {answer_a}\nB: {answer_b}")
+    # try:
+    #     submit_answer(answer_a, "a", day=15, year=2019)
+    # except AocdError:
+    #     submit_answer(answer_b, "b", day=15, year=2019)
